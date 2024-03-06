1f8c43bc4f82365a3278fcfbf0a7b4987d080f3f
arkaprav <chakraarkaprava@gmail.com>
2024-03-06 12:47:41 +0530

    changed banner styling

--- src/components/Herobanner/Herobanner.jsx
+++ src/components/Herobanner/Herobanner.jsx
@@ -1,38 +1,7 @@
 /* eslint-disable no-unused-vars */
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIosNew";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useState, useEffect } from "react";
-import { Carousel } from "react-responsive-carousel";
 
 function Herobanner() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-    // setCurrentIndex((prevSlide) =>
-    //   prevSlide === 0 ? slides.length - 1 : prevSlide - 1
-    // );
-  };
-
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-    // setCurrentIndex((prevSlide) => (prevSlide + 1) % slides.length);
-  };
-
-  useEffect(() => {
-    // Automatically advance to the next slide every 5000ms (5 seconds)
-    const intervalId = setInterval(() => {
-      nextSlide();
-    }, 10000);
-
-    // Cleanup the interval when the component unmounts
-    return () => clearInterval(intervalId);
-  }, [currentIndex]);
-
   const slides = [
     {
       url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
